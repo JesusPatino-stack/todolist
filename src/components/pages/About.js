e4d5771b47f4afd56cb700da158b6746c8d75914
Jesus Patino <ckf782@mocs.utc.edu>
2020-02-19 17:29:56 -0500

    testing git

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>Do</h1>
+      <p>Empty Fragment Placeholder for new features</p>
+    </React.Fragment>
+  )
+}
+
+
+export default About;
